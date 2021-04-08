1cb7819634641962a66dbcc8241cd5544f22615f
mason <mason.whitaker79@gmail.com>
2021-04-08 08:51:16 -0700

    LALALALALA kitten.artime

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-'use strict'
+'use strict';
 
-Person(name, age, email, nickname); {
-    this.name = name
-    this.age = age
-    this.email = email
-    this.nickname = nickname 
-
-};
-me_person = new Person('Mason', 15, 'mason@mason.io', 'the Jar')
-
+function Person(name_param, age, email, nickname) {
+    this.name = name_param;
+    this.age = age;
+    this.email = email;
+    this.nickname = nickname;
+}
 Person.prototype.render = function() {
-    let mySection = document.getElementById('mySection')
+    let mainSection = document.getElementById('mainSection')
 
-    let nameH2 = document.createElement('h2')
-    let ageH2 = document.createElement('h2')
-    let nicknameH2 = document.createElement('h2')
+    let card = document.createElement('section');
+    card.className = 'card';
+    let nameH2 = document.createElement('h2');
+    let ageP = document.createElement('p');
+    let emailP = document.createElement('p');
+    let nickNameP = document.createElement('p');
 
-    nameH2.textContent = 'Name: ' + this.name
-    ageH2.textContent = 'Age: ' + this.age
-    nicknameH2.textContent = 'Nickname: ' + this.nickname
+    nameH2.textContent = this.name;
+    ageP.textContent = 'Age: ' + this.age;
+    emailP.textContent = 'Email: ' + this.email;
+    nickNameP.textContent = 'Nickname: ' + this.nickname;
 
-    mySection.appendChild(nameH2);
-    mySection.appendChild(ageH2);
-    mySection.appendChild(nicknameH2);
+    card.appendChild(nameH2);
+    card.appendChild(ageP);
+    card.appendChild(emailP);
+    card.appendChild(nickNameP);
+    mainSection.appendChild(card);
 };
-
-Person.render();
+let kitten = new Person('Kiteen', 50, 'mewmewmew@mewmewmew.artime', 'Kitten LaRue');
+kitten.render();
